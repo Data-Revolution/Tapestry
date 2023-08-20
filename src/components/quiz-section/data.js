00d29ec75f8c7e1ab2d5938622f943a446903f8b
Alisha <alisha.k3226@gmail.com>
2023-08-20 14:59:12 +0930

    feat: add images to quiz

--- src/components/quiz-section/data.js
+++ src/components/quiz-section/data.js
@@ -5,18 +5,21 @@ export const quiz = {
       id: 1,
       question:
         'Which South Australian city hosts the annual "Mad March" events including the Adelaide Fringe Festival, WOMADelaide, and the Clipsal 500?',
+      image: '/images/q1.jpg',
       answers: ['Port Augusta ', 'Whyalla', 'Adelaide'],
       correctAnswer: 'Adelaide',
     },
     {
       id: 2,
       question: 'What natural phenomenon occurs at the Limestone Coasts "Blue Lake" that draws visitors each year?',
+      image: '/images/q2.jpg',
       answers: ['Bioluminescence', 'Colour-changing water', 'Underwater Caves'],
       correctAnswer: 'Colour-changing water',
     },
     {
       id: 3,
       question: 'Which South Australian coastal town is famous for its great white shark cage diving opportunities?',
+      image: '/images/q3.jpg',
       answers: ['Port Lincoln', 'Glenelg', 'Robe'],
       correctAnswer: 'Port Lincoln',
     },
@@ -24,12 +27,14 @@ export const quiz = {
       id: 4,
       question:
         'Which landmark is the only place on Earth where 350 million years of near-continuous geological sequence can be seen?',
+      image: '/images/q4.jpg',
       answers: ['Uluru', 'Flinders Ranges', 'Kangaroo Island'],
       correctAnswer: 'Flinders Ranges',
     },
     {
       id: 5,
       question: 'Where in South Australia are the Australian Giant Cuttlefish found the most?',
+      image: '/images/q5.jpg',
       answers: ['Oak Valley ', 'Streaky Bay', 'Whyalla'],
       correctAnswer: 'Whyalla',
     },
